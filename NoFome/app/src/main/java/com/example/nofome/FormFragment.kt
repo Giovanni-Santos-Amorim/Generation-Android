a420178383553a423ac023be072464fe3bbfbcfa
Giovanni Santos Amorim <giovanni2401@hotmail.com>
2022-06-14 15:44:01 -0300

    NoFome

--- NoFome/app/src/main/java/com/example/nofome/FormFragment.kt
+++ NoFome/app/src/main/java/com/example/nofome/FormFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.nofome.databinding.FragmentFormBinding
 
@@ -22,9 +23,36 @@ class FormFragment : Fragment() {
         binding = FragmentFormBinding.inflate(layoutInflater, container, false)
 
         binding.btnSave.setOnClickListener {
-            findNavController().navigate(R.id.action_formFragment_to_listFragment)
+            insert()
         }
 
         return binding.root
     }
+
+    private fun validateField(
+        name: String,
+        description: String,
+        value: String,
+    ): Boolean{
+        return !((name == ""|| name.length > 20)||
+                description == "" ||
+                value == "")
+    }
+
+    private fun insert(){
+        val name = binding.ptName.text.toString()
+        val description = binding.ptDescription.text.toString()
+        val value = binding.ptValue.text.toString()
+
+        if(validateField(name, description, value)){
+            Toast.makeText(context, "Cadastrad com sucesso", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_formFragment_to_listFragment)
+        }
+
+        else{
+            Toast.makeText(context, "Cadastrad com sucesso", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
